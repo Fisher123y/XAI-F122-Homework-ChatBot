900f99a53a40c8d3cbbc44adf012b886f57d21bc
yysCyber <2161624113@qq.com>
2022-01-12 00:20:36 +0200

    Commit ChatBot 1-st Stage

--- src/nix/education/java/chatbot/ChatBot.java
+++ src/nix/education/java/chatbot/ChatBot.java
@@ -1,7 +1,29 @@
 package nix.education.java.chatbot;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class ChatBot {
 
+    private String chatBotName;
 
+    private String chatBotBirthYear;
+
+    private static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy");
+
+    public ChatBot(String chatBotName) {
+        this.chatBotName = chatBotName;
+        this.chatBotBirthYear = DATE_FORMAT.format(new Date());
+    }
+
+    public void run() {
+        stage1();
+    }
+
+    private void stage1() {
+        System.out.println("Hello! My name is " + this.chatBotName + ".");
+        System.out.println("I was created in " + this.chatBotBirthYear + ".");
+    }
 
 }
